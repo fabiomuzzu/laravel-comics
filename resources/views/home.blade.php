f3cb17916235863db3209914d8d7e4bdea5a776a
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-02-12 16:56:30 +0100

    added assets

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Laravel Comics</title>
+    @vite('resources/js/app.js')
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">Ciao</h1>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
